fa98435fef8e32303ba67d440e2a82013dd16abf
mukund <hirparamukund@gmail.com>
2021-01-18 18:15:39 +0530

    init design project

---
+++ office/index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Son Joaquin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- link Font File -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <!-- top-bar Section Start -->
+    <section class="top-bar-section">
+        <div class="row">
+            <div class="col-lg-6 text-left justify-content-center">
+                <div class="top-bar-left">
+                    <ul>
+                        <li>
+                            <a href="#" rel="noopener noreferrer">
+                                <img src="assets/images/icon_1.png" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" rel="noopener noreferrer">
+                                <img src="assets/images/icon_2.png" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" rel="noopener noreferrer">
+                                <img src="assets/images/icon_3.png" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" rel="noopener noreferrer">
+                                <img src="assets/images/icon_4.png" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6 text-center">
+                <div class="top-bar-right">
+                    <ul>
+                        <li>
+                            <a href="#" rel="noopener noreferrer">
+                                <img src="assets/images/Mask-Group-12.svg" alt="">
+                                <span>Pay Online</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" rel="noopener noreferrer">
+                                <img src="assets/images/Mask-Group-11.svg" alt="">
+                                <span>Special Offers</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- top-bar Section end -->
+
+    <!-- logo bar Start -->
+    <section class="logo-bar-section">
+        <div class="container">
+            <div class="row logo-bar">
+                <div class="col-lg-4">
+                    <img src="assets/images/logo..png" alt="">
+                </div>
+                <div class="col-lg-4">
+                    <div class="contect-text text-center">
+                        <p>Call us today for a Free Quote</p>
+                        <p class="c-red">(661) 367-8636</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 logo-bar-3">
+                    <p class="f-w-700 f-size-30">WEED PROBLEMS ?</p>
+                    <p class="f-w-700 f-size-30">LAWN CARE/SHRUBS</p>
+                    <p class="f-size-13">The Experienced Gardener Can Help</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- logo bar end -->
+
+    <!-- Nav-menu start -->
+    <section class="nav-section">
+        <div class="container">
+            <div class="row nav-items">
+                <div class="navbar navbar-expand-lg">
+                    <ul>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Residential Pest</a>
+                        </li>
+                        <li>
+                            <a href="#">Commercial Pest</a>
+                        </li>
+                        <li>
+                            <a href="#">Area We Serve</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Nav-menu end -->
+
+    <section class="baner-section">
+        <div class="container">
+            <div class="bannar-row d-inline-block">
+                <p class="banner-heading">Kid & Pet Friendly <br /> Pest Control</p>
+                <p class="banner-content">Starting at $42 per month !</p>
+                <a href="#" class="btn btn-danger">Schedule Today</a>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row banner-text">
+            <div class="col-lg-1">
+                <p>Get A FREE Estimate</p> 
+            </div>
+            <div class="col-lg-9">
+                <ul>
+                    <li>
+                        <input type="text" placeholder="Name">
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Email">
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Phone">
+                    </li>
+                    <li>
+                        <select name="service">
+                            <option value="">Service</option>
+                            <option value=""></option>
+                            <option value=""></option>
+                        </select>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-danger">Send</a>
+                    </li>
+                </ul> 
+                <p>By submitting this from, you are agreeing to the privacy policy</p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
